17fc9c60938f019177ac514c029f7d2f3281c8f1
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-09-23 14:31:24 -0400

    Add loadDashboard function to populate DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,11 +2,15 @@
 // const picker = datepicker(selector, options)
 
 import { validateUserLogin } from "./login"
-import { getAllData } from "./scripts"
+
+import { getAllData, currentCustomer, totalBookings, totalRooms } from "./scripts"
 
 // loadDashboard function 
 // - use params: current customer, bookings, and rooms
 // if login successful, call a function that updates the user's bookings 
+
+const customerLoginForm = document.querySelector("form")
+const bookingContainer = document.querySelector(".current-bookings-container")
 
   //Helper Functions👇
   const removeHiddenClass = (elements) => {
@@ -23,13 +27,27 @@ import { getAllData } from "./scripts"
     return elements;
   };
 
+
+
+  export const displayBookingCards = (customerBookings) => {
+    console.log(customerBookings)
+
+
+
+    // 1. Get query selector for the container where I want to add the booking cards
+// 2. Get the data for the booking cards
+// 3. Populate the html with the booking data:
+// — For each booking, create a booking card
+
+  }
+
   // addHiddenClass([]);
 //   removeHiddenClass([])
 
-const handleLogin = () => {
-  validateUserLogin(username, password, customerData)
-  getAllData()
-}
+// const handleLogin = () => {
+//   validateUserLogin(username, password, customerData)
+//   getAllData()
+// }
 
 
 
@@ -37,6 +55,16 @@ const handleLogin = () => {
 
 
 
+// bookingContainer.innerHTML += `
+// <section class="room-card">
+//           <div class="room-card-container">
+//             <p class="booking-info" id="1">Room Number:1</p>
+//             <p class="booking-info">Cost: $350</p>
+//             <p class="booking-info">Room Type: Suite</p>
+//             <p class="booking-info">Beds: 1 King Size Bed</p>
+//           </div>
+//         </section>
+// `
 
 
 
--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,26 @@
 // IMPORT
 import './css/styles.css';
 import { getCustomers, getBookings, getRooms } from "./apiCalls" // Import GET functions from apiCalls.js
-import {} from "./domUpdates" // Import DOM functions that require the actual data when they are called inside of event listeners 
+import {displayBookingCards } from "./domUpdates" // Import DOM functions that require the actual data when they are called inside of event listeners 
 // i.e. load dashboard, render filter results, and book functions 
+import { storeCustomerBookings } from "./existing-bookings"
+
 
 
 // QUERY SELECTORS
 // add all query selectors here. EXPORT them, then IMPORT in domUpdates 
-const customerLoginForm = document.querySelector("form")
+
 
 // GLOBAL VARIABLES 
-let currentCustomer
-let totalBookings
-let totalRooms
+export let currentCustomer
+export let totalBookings
+export let totalRooms
 // declare global variables that will store the actual data and be passed into functions that update the DOM.
 
 
 // START FUNCTION
-const getAllData = () => { // call getAllData inside a function that runs when a user logs in...?
-  Promise.all([getCustomers(), getBookings(), getRooms()])
+export const getAllData = () => { // call getAllData inside a function that runs when a user logs in...?
+  return Promise.all([getCustomers(), getBookings(), getRooms()])
   .then( data =>{
     console.log(data)
     currentCustomer = data[0].customers
@@ -29,8 +31,13 @@ const getAllData = () => { // call getAllData inside a function that runs when a
 
 
 // EVENT LISTENERS
+const loadDashboard = () => {
+  getAllData().then( () => {
+    const customerBookings = storeCustomerBookings(currentCustomer[1], totalBookings, totalRooms)
+    displayBookingCards(customerBookings)
+  })
+}
 
-customerLoginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  validateUserLogin()
-});
+window.addEventListener("load", loadDashboard)
+
+
