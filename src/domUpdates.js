184ccb5f7104a22f30a00aee86f07a6d36b94095
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-09-26 09:49:43 -0400

    Update login display

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,52 +1,49 @@
-import { validateUserLogin } from "./login"
+import { validateUserLogin } from "./login";
 
-import { getRoomAvailability } from "./available-rooms"
-import { calculateTotalRoomCost } from "./booked-rooms"
+import { getRoomAvailability } from "./available-rooms";
+import { calculateTotalRoomCost } from "./booked-rooms";
 // import { filterByRoomType } from "./filter-rooms"
 // QUERY SELECTORS
-const loginView = document.querySelector("#login-view")
-const customerLoginForm = document.querySelector("form")
-const usernameInput = document.querySelector("#username-input")
-const passwordInput = document.querySelector("#password-input")
-export const loginSubmitBtn = document.querySelector("#login-submit-btn")
-const usernameDisplay = document.querySelector("#username-display")
+const loginView = document.querySelector("#login-view");
+const customerLoginForm = document.querySelector("form");
+const usernameInput = document.querySelector("#username-input");
+const passwordInput = document.querySelector("#password-input");
+export const loginSubmitBtn = document.querySelector("#login-submit-btn");
+const usernameDisplay = document.querySelector("#username-display");
 
-const currentBookingContainer = document.querySelector(".current-bookings-container")
-export const availableRoomsContainer = document.querySelector(".available-rooms-container")
-const bookingsTotal = document.querySelector(".total-spent")
-export const bookingBtn = document.querySelector("#new-booking")
-export const dashboardBtn = document.querySelector("#dashboard-btn")
-export const searchDateBtn = document.querySelector("#search-btn")
-export const selectedDate = document.querySelector("#selected-date-input")
-const dashboardView = document.querySelector("#dashboard-view")
-const newBookingView = document.querySelector("#new-bookings-view")
-export const roomTypeDropdown = document.querySelector("#room-type-dropdown")
-export const roomTypeSelection = document.querySelector("#room-type")
+const currentBookingContainer = document.querySelector(".current-bookings-container");
+export const availableRoomsContainer = document.querySelector(".available-rooms-container");
+const bookingsTotal = document.querySelector(".total-spent");
+export const bookingBtn = document.querySelector("#new-booking");
+export const dashboardBtn = document.querySelector("#dashboard-btn");
+export const searchDateBtn = document.querySelector("#search-btn");
+export const selectedDate = document.querySelector("#selected-date-input");
+const dashboardView = document.querySelector("#dashboard-view");
+const newBookingView = document.querySelector("#new-bookings-view");
+export const roomTypeDropdown = document.querySelector("#room-type-dropdown");
+export const roomTypeSelection = document.querySelector("#room-type");
 // export const bookingMessage = document.querySelector("#booking-message")
 
+//Helper Functions👇
+export const removeHiddenClass = (elements) => {
+  elements.forEach((element) => {
+    element.classList.remove("hidden");
+  });
+  return elements;
+};
 
-  //Helper Functions👇
-  export const removeHiddenClass = (elements) => {
-    elements.forEach(element => {
-      element.classList.remove('hidden');
-    });
-    return elements;
-  };
-
-  export const addHiddenClass = (elements) => {
-    elements.forEach(element => {
-      element.classList.add('hidden');
-    });
-    return elements;
-  };
-
+export const addHiddenClass = (elements) => {
+  elements.forEach((element) => {
+    element.classList.add("hidden");
+  });
+  return elements;
+};
 
 // DASHBOARD
-  export const renderBookingCards = (customerBookings) => {
-      addHiddenClass([]);
-    currentBookingContainer.innerHTML = " "
-    customerBookings.forEach((booking) => {
-      currentBookingContainer.innerHTML += `
+export const renderBookingCards = (customerBookings) => {
+  currentBookingContainer.innerHTML = " ";
+  customerBookings.forEach((booking) => {
+    currentBookingContainer.innerHTML += `
       <article class="room-card" id="${booking.room.number}>
               <ul class="room-card-container">
                 <li class="booking-info">Room Number: ${booking.room.number}</li>
@@ -56,35 +53,36 @@ export const roomTypeSelection = document.querySelector("#room-type")
                 <li class="booking-info">Date: ${booking.date}</li>
                 </ul>
             </article>
-      `
-    })
-  }
+      `;
+  });
+};
 
-  export const renderBookingsTotal = (customerBookings) => {
-    bookingsTotal.innerHTML = `Total Spent: $${calculateTotalRoomCost(customerBookings).toFixed(2)}`;
-  }
+export const renderBookingsTotal = (customerBookings) => {
+  bookingsTotal.innerHTML = `Total Spent: $${calculateTotalRoomCost(customerBookings).toFixed(2)}`;
+};
 
 // AVAILABLE BOOKINGS
 
-export const displayBookingsView = () => {
-  addHiddenClass([dashboardView]);
-  removeHiddenClass([newBookingView])
-}
+export const displayLoginView = () => {
+  removeHiddenClass([dashboardView, bookingBtn, dashboardBtn]);
+  addHiddenClass([loginView, newBookingView]);
+};
 
 export const displayDashboardView = () => {
-  removeHiddenClass([dashboardView])
-  addHiddenClass([newBookingView]);
-}
-
-export const displayLoginView = () => {
   removeHiddenClass([dashboardView]);
-  addHiddenClass([loginView, newBookingView]);
+  addHiddenClass([newBookingView]);
+};
 
-}
+export const displayBookingsView = () => {
+  addHiddenClass([dashboardView]);
+  removeHiddenClass([newBookingView]);
+};
+
+
 
 export const displayAvailableRooms = (availableRooms) => {
-  console.log({availableRooms})
-  availableRoomsContainer.innerHTML = ""
+  console.log({ availableRooms });
+  availableRoomsContainer.innerHTML = "";
   availableRooms.forEach((room) => {
     availableRoomsContainer.innerHTML += `
     <article class="room-card" id="${room.number}">
@@ -96,34 +94,23 @@ export const displayAvailableRooms = (availableRooms) => {
               </ul>
               <button id="book-now-btn" class="book-now">Book Now</button>
           </article>
-    `
-  })
-}
-
-export const updateAvailableRooms = (roomsData, bookingsData, searchDate, selectedRoomType) => {
-  const availableRooms = getRoomAvailability(roomsData, bookingsData, searchDate, selectedRoomType);
-  displayAvailableRooms(availableRooms)
+    `;
+  });
 };
 
+export const updateAvailableRooms = (
+  roomsData,
+  bookingsData,
+  searchDate,
+  selectedRoomType
+) => {
+  const availableRooms = getRoomAvailability(roomsData, bookingsData, searchDate, selectedRoomType);
+  displayAvailableRooms(availableRooms);
+};
 
 export const handleLogin = (currentCustomer) => {
-const username = usernameInput.value
-const password = passwordInput.value 
-const validatedCustomer = validateUserLogin(username, password, currentCustomer)
-usernameDisplay.innerText = validatedCustomer.name
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const validatedCustomer = validateUserLogin(username, password, currentCustomer);
+  usernameDisplay.innerText = validatedCustomer.name;
+};
