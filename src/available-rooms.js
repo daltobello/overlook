ebc5c987ec94b3d9ad0de167dfcb65360e714809
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-09-26 07:28:11 -0400

    Update getRoomAvailability to filter all

--- src/available-rooms.js
+++ src/available-rooms.js
@@ -1,27 +1,21 @@
 import { filterByRoomType } from "./filter-rooms"
 
 export const getRoomAvailability = (roomsData, bookingsData, searchDate, roomType) => {
-
-  console.log("ROOMS DATA:", roomsData)
-  console.log("BOOKINGS DATA:", bookingsData)
-  console.log("SEARCH:", searchDate)
   const bookingsOnSearchDate = bookingsData.filter((booking) => {
-
-    
     return booking.date === searchDate
   })
   const availableRooms = roomsData.filter((room) => { 
     const isRoomBooked = bookingsOnSearchDate.some((booking) => { 
      return  booking.roomNumber === room.number 
     })
-    console.log("isRoomBooked", isRoomBooked)
     return !isRoomBooked 
   })
-  console.log({availableRooms})
-  console.log({roomType})
-  const filteredByType = filterByRoomType(availableRooms, roomType)
-  console.log("AFTER INVOCATION", filteredByType)
-  return filteredByType
+  if (roomType === "all") {
+    return availableRooms
+  } else {
+    const filteredByType = filterByRoomType(availableRooms, roomType)
+    return filteredByType
+  }
 }
 
 
@@ -30,18 +24,10 @@ export const getRoomAvailability = (roomsData, bookingsData, searchDate, roomTyp
       // - isRoomBooked returns false if room available on the searchDate
       
  // bookingsOnSearchDate array only contains bookings with the searchDate
-        // const formattedSearchDate = searchDate.split("/").join("-")
+// const formattedSearchDate = searchDate.split("/").join("-")
 
 
 
 
 
-// cont addBooking = (currentCustomer) => {
-//   // sequence of what happens when button is clicked
-//   // avail rooms sitting on DOM, when you hit 
-//   const currentCustomerBookings = getCustomerBookings()
-//   const availRooms = getRoomAvailability()
-//   currentCustomerBookings.push(availRooms)
-//   // push into customer booking to add bookings
-// }
 
